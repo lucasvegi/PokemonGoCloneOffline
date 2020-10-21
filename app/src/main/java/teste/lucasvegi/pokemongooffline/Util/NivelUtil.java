e85626e74577642b81175f55a7eeabc496db1974
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-10-21 11:02:15 -0300

    Atualizando classe NivelUtil

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Util/NivelUtil.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Util/NivelUtil.java
@@ -1,4 +1,59 @@
 package teste.lucasvegi.pokemongooffline.Util;
 
+import android.content.ContentValues;
+
+import teste.lucasvegi.pokemongooffline.Model.ControladoraFachadaSingleton;
+import teste.lucasvegi.pokemongooffline.Model.Usuario;
+
 public class NivelUtil {
+
+	public static void aumentaXp(String evento) {
+		final Usuario usuario = ControladoraFachadaSingleton.getInstance().getUsuario();
+		final int xpRecebido = getXpEvento(evento);
+		final int nivelAtual = usuario.getNivel();
+		final int xpAtual = usuario.getXp();
+		final int xpMax = xpMaximo(nivelAtual);
+		int xpFinal = xpAtual, nivelFinal = nivelAtual;
+
+		if((xpAtual + xpRecebido) >= xpMax) {
+			xpFinal = (xpAtual + xpRecebido) - xpMax;
+			nivelFinal++;
+			usuario.setNivel(nivelFinal);
+		} else {
+			xpFinal = xpAtual + xpRecebido;
+		}
+
+		usuario.setXp(xpFinal);
+
+		ContentValues valores = new ContentValues();
+		valores.put("login", usuario.getLogin());
+		valores.put("senha", usuario.getSenha());
+		valores.put("nome", usuario.getNome());
+		valores.put("sexo", usuario.getSexo());
+		valores.put("foto", usuario.getFoto());
+		valores.put("dtCadastro", usuario.getDtCadastro());
+		valores.put("temSessao", "");
+		valores.put("nivel", nivelFinal);
+		valores.put("xp", xpFinal);
+		BancoDadosSingleton.getInstance().atualizar("Usuario", valores, "login = " + usuario.getLogin());
+	}
+
+	public static int xpMaximo(int nivelUsuario) {
+		return nivelUsuario*1000;
+	}
+
+	private static int getXpEvento(String evento) {
+		switch(evento) {
+			case "captura":
+				return 20;
+			case "evolui":
+				return 200;
+			case "pokestop":
+				return 50;
+			case "choca":
+				return 100;
+			default:
+				return 0;
+		}
+	}
 }
