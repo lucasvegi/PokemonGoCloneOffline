a54e835c4161501123aff54cc2f51ac675a4aedf
wallace-f-rosa <wferancini@gmail.com>
2020-10-24 15:19:13 -0300

    construtor public e método para retornar opções de marcador

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Model/Pokestop.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Model/Pokestop.java
@@ -5,6 +5,9 @@ import android.graphics.Bitmap;
 import android.util.Log;
 
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.FetchPlaceRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -15,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import teste.lucasvegi.pokemongooffline.R;
 import teste.lucasvegi.pokemongooffline.Util.BancoDadosSingleton;
 
 /**
@@ -23,7 +27,7 @@ import teste.lucasvegi.pokemongooffline.Util.BancoDadosSingleton;
 public class Pokestop implements Serializable{
     private String id;
     private String nome;
-    private Bitmap foto;
+    private Bitmap foto = null;
     private Double lat;
     private Double longi;
     private String descri;
@@ -33,7 +37,7 @@ public class Pokestop implements Serializable{
 
     }
 
-    protected Pokestop(String ID, String Name) {
+    public Pokestop(String ID, String Name) {
         this.id = ID;
         this.nome = Name;
         //this.foto = Photo;
@@ -96,5 +100,46 @@ public class Pokestop implements Serializable{
         this.UltimoAcesso = tempo;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLongi() {
+        return longi;
+    }
+
+    public void setLongi(Double longi) {
+        this.longi = longi;
+    }
+
+    public MarkerOptions getMarkerOptions(boolean interactionPossible){
+        MarkerOptions markeropt = new MarkerOptions();
+
+        if (interactionPossible) {
+            markeropt.icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.pokestop_perto))
+                .position(new LatLng(lat, longi))
+                .title(nome)
+                .alpha(3);
+        } else {
+            markeropt.icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.pokestop_longe))
+                .position(new LatLng(lat, longi))
+                .title(nome)
+                .alpha(3);
+        }
+        return markeropt;
+    }
 }
