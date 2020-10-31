9b62f1aabde1c2639a93a7271703aa191abb4f8d
mellofe <61694269+mellofe@users.noreply.github.com>
2020-10-31 18:27:51 -0300

    Alteração nomes dos ovos

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/OvosActivity.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/OvosActivity.java
@@ -1,10 +1,25 @@
 package teste.lucasvegi.pokemongooffline.Controller;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 
-public class OvosActivity extends Activity {
+import teste.lucasvegi.pokemongooffline.R;
+
+public class OvosActivity extends Activity implements AdapterView.OnItemClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ovos);
+    }
+
     public void clickVoltar(View v){
         finish();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
