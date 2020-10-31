f2cfbdcc706658938b0c2930678b1b7c83eec5ff
felipemello <>
2020-10-31 11:51:09 -0300

    Criação do layout da página de ovos

---
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/OvosActivity.java
@@ -0,0 +1,10 @@
+package teste.lucasvegi.pokemongooffline.Controller;
+
+import android.app.Activity;
+import android.view.View;
+
+public class OvosActivity extends Activity {
+    public void clickVoltar(View v){
+        finish();
+    }
+}
