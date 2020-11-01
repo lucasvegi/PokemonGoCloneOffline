4da67fb624c437cc9e1b0ae082830f23b5daa83f
mellofe <61694269+mellofe@users.noreply.github.com>
2020-10-31 22:47:51 -0300

    Criação dos elementos responsáveis pela lista de ovos

--- app/src/main/java/teste/lucasvegi/pokemongooffline/View/AdapterOvos.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/View/AdapterOvos.java
@@ -1,36 +1,79 @@
 package teste.lucasvegi.pokemongooffline.View;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
-import teste.lucasvegi.pokemongooffline.Controller.OvosActivity;
 import teste.lucasvegi.pokemongooffline.Model.Ovo;
+import teste.lucasvegi.pokemongooffline.R;
 
 public class AdapterOvos extends BaseAdapter {
-    public AdapterOvos(List<Ovo> ovos, OvosActivity ovosActivity) {
 
+    private List<Ovo> ovos;
+    private Activity act;
+
+    public AdapterOvos(List<Ovo> ovos, Activity act) {
+        try {
+            this.ovos = ovos;
+            this.act = act;
+        } catch (Exception e) {
+            Log.e("OVO", "ERRO: " + e.getMessage());
+        }
     }
 
     @Override
-    public int getCount() {
-        return 0;
+    public int getCount() { return ovos.size(); }
+
+    @Override
+    public Object getItem(int position) {
+        return ovos.get(position);
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public long getItemId(int position) {
+        return ovos.get(position).getIdOvo();
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        try {
+            View view = act.getLayoutInflater().inflate(R.layout.lista_ovos_personalizada, parent, false);
 
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+            Ovo ovo = ovos.get(position);
+
+            Log.i("OVOS", "Montando lista de ovos para " + ovo.getCor());
+
+            ImageView imagem = (ImageView)
+                    view.findViewById(R.id.imagemOvoOvos);
+
+            TextView kmAndou = (TextView)
+                    view.findViewById(R.id.kmAndou);
+
+            Button botaoAndou = (Button)
+                    view.findViewById(R.id.botaoIncubar);
+
+
+            //Decide se vai ter informações do pokemon ou não
+            if(ovo.getIdOvo() != 0) {
+                kmAndou.setText("0km");
+                imagem.setImageResource(ovo.getFoto());
+
+            }else {
+                kmAndou.setText("");
+                imagem.setImageResource(R.drawable.help);
+            }
+
+            return view;
+        }catch (Exception e){
+            Log.e("OVO", "ERRO: " + e.getMessage());
+            return null;
+        }
     }
 }
