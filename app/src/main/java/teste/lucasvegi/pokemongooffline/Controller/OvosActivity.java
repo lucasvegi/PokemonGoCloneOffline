579802cf0ec8837fb475cf1607ebe92d09bbe40c
mellofe <61694269+mellofe@users.noreply.github.com>
2020-11-25 15:06:34 -0300

    Musica tocando na ovos activity

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/OvosActivity.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/OvosActivity.java
@@ -27,56 +27,58 @@ public class OvosActivity extends Activity implements AdapterView.OnItemClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ovos);
+        try {
+            TextView txtTotal = (TextView) findViewById(R.id.txtOvosTotal);
 
-        TextView txtTotal = (TextView) findViewById(R.id.txtOvosTotal);
 
+            ovos = ControladoraFachadaSingleton.getInstance().getOvos();
+            ListView listView = (ListView) findViewById(R.id.listaOvos);
 
-        ovos = ControladoraFachadaSingleton.getInstance().getOvos();
-        ListView listView = (ListView) findViewById(R.id.listaOvos);
+            Intent it = getIntent();
+            Location localizacaoAtual = it.getParcelableExtra("location");
 
-        Intent it = getIntent();
-        Location localizacaoAtual = it.getParcelableExtra("location");
-
-        for (int i = 0; i < ovos.size(); i++) {
-            if (ovos.get(i).getLocalizacao() == null) {
-                ovos.get(i).setLocalizacao(localizacaoAtual);
-            } else {
-                if (ovos.get(i).getIncubado() == 1) {
-                    double distancia = localizacaoAtual.distanceTo(ovos.get(i).getLocalizacao()) / 1000;
-                    Log.i("OVOS", "Distância: " + distancia);
-                    ovos.get(i).setKmAndado(ovos.get(i).getKmAndado() + distancia);
+            for (int i = 0; i < ovos.size(); i++) {
+                if (ovos.get(i).getLocalizacao() == null) {
                     ovos.get(i).setLocalizacao(localizacaoAtual);
-                    ControladoraFachadaSingleton.getInstance().setKmAndado(ovos.get(i).getIdOvo(), ovos.get(i).getKmAndado());
+                } else {
+                    if (ovos.get(i).getIncubado() == 1) {
+                        double distancia = localizacaoAtual.distanceTo(ovos.get(i).getLocalizacao()) / 1000;
+                        Log.i("OVOS", "Distância: " + distancia);
+                        ovos.get(i).setKmAndado(ovos.get(i).getKmAndado() + distancia);
+                        ovos.get(i).setLocalizacao(localizacaoAtual);
+                        ControladoraFachadaSingleton.getInstance().setKmAndado(ovos.get(i).getIdOvo(), ovos.get(i).getKmAndado());
 
-                    //testa se ovo chocou
-                    if (ovos.get(i).getKmAndado() >= ovos.get(i).getKm()) {
-                        ovos.get(i).setChocado(1);
-                        ControladoraFachadaSingleton.getInstance().setChocado(ovos.get(i).getIdOvo(), 1);
-                        String nome = ControladoraFachadaSingleton.getInstance().getNomePokemonOvo(ovos.get(i).getIdOvo());
-                        Toast.makeText(this, "Chocou um " + nome, Toast.LENGTH_LONG).show();
+                        //testa se ovo chocou
+                        if (ovos.get(i).getKmAndado() >= ovos.get(i).getKm()) {
+                            ovos.get(i).setChocado(1);
+                            ControladoraFachadaSingleton.getInstance().setChocado(ovos.get(i).getIdOvo(), 1);
+                            String nome = ControladoraFachadaSingleton.getInstance().getNomePokemonOvo(ovos.get(i).getIdOvo());
+                            Toast.makeText(this, "Chocou um " + nome, Toast.LENGTH_LONG).show();
 
-                        //ovo já foi exibido
-                        ControladoraFachadaSingleton.getInstance().setExibido(ovos.get(i).getIdOvo(), 1);
+                            //ovo já foi exibido
+                            ControladoraFachadaSingleton.getInstance().setExibido(ovos.get(i).getIdOvo(), 1);
 
-                        //remove ovo da lista de ovos
-                        Ovo o = ovos.get(i);
-                        ovos.remove(o);
+                            //remove ovo da lista de ovos
+                            Ovo o = ovos.get(i);
+                            ovos.remove(o);
 
+                        }
                     }
                 }
             }
+            //total de ovos na lista
+            int total = ControladoraFachadaSingleton.getInstance().getOvos().size();
+            txtTotal.setText("Ovos: " + total + "/9");
+
+            AdapterOvos adapterOvos = new AdapterOvos(ovos, this, localizacaoAtual);
+            listView.setAdapter(adapterOvos);
+            listView.setOnItemClickListener(this);
+            mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.tema_menu);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }catch (Exception e){
+            Log.e("OVOS", "ERRO: " + e.getMessage());
         }
-
-        //total de ovos na lista
-        int total = ControladoraFachadaSingleton.getInstance().getOvos().size();
-        txtTotal.setText("Ovos: " + total + "/9");
-
-        AdapterOvos adapterOvos = new AdapterOvos(ovos, this, localizacaoAtual);
-        listView.setAdapter(adapterOvos);
-        listView.setOnItemClickListener(this);
-        mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.tema_menu);
-        mediaPlayer.setLooping(true);
-        mediaPlayer.start();
     }
     @Override
     protected void onDestroy() {
